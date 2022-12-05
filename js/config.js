f411eee0fa788e8c4a02a9de6d49c91b3c07fa58
Emmaleary01 <112977270+Emmaleary01@users.noreply.github.com>
2022-12-04 20:33:31 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId:         ' us-west-1_zjH3DEwOW',          //  e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '65bbggcmskngcun0md5c5bdi8q',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId:   '3cdm9j6aasguhc1onl4roha0qf',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-west-1'                     // e.g. us-east-2
     },
     api: {
