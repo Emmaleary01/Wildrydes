c3a2c2d1c041a9bb84da8b4e59b8ad15dd1885f1
Emmaleary01 <112977270+Emmaleary01@users.noreply.github.com>
2022-12-05 15:44:45 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         ' us-west-1_zjH3DEwOW',          //  e.g. us-east-2_uXboG5pAb
+        userPoolId:         'us-west-1_zjH3DEwOW',          //  e.g. us-east-2_uXboG5pAb
         userPoolClientId:   '3cdm9j6aasguhc1onl4roha0qf',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-west-1'                     // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ' https://ebmhk13pc2.execute-api.us-west-1.amazonaws.com/prod'  //  https://bswoadnczc.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://ebmhk13pc2.execute-api.us-west-1.amazonaws.com/prod'  //  https://bswoadnczc.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
